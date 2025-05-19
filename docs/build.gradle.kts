34361831fa68537aa11dcbbf1664333e3ebd0905
stephengold <sgold@sonic.net>
2025-05-19 13:13:17 -0700

    buildscripts:  copy v1.0.0 javadoc to the site

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -2,10 +2,32 @@
 
 plugins {
     java
+    alias(libs.plugins.download) // to retrieve files from URLs
 }
+
+import de.undercouch.gradle.tasks.download.Download
 
 sourceSets.main {
     java {
         srcDir("en") // for IDE access (no Java there)
     }
+}
+
+// Register javadoc-to-(web)site tasks, triggered by push-master.yml:
+
+val artifactId = "jolt-jni-Windows64"
+val centralUrl = "https://repo1.maven.org/maven2/com/github/stephengold"
+val jarVersion = "1.0.0"
+val jarName = artifactId + "-" + jarVersion + "-javadoc.jar"
+val downloadUrl = centralUrl + "/" + artifactId + "/" + jarVersion + "/" + jarName
+
+tasks.register<Copy>("copyJavadocToSite") {
+    dependsOn("downloadJavadocJar")
+    from(zipTree(jarName))
+    into("build/site/javadoc/latest")
+}
+
+tasks.register<Download>("downloadJavadocJar") {
+    dest(file(jarName))
+    src(downloadUrl)
 }
