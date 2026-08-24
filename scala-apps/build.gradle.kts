7cd7cf21e0e060d418aceb8e0292cb212ade457b
stephengold <sgold@sonic.net>
2026-08-24 09:52:40 -0700

    scala-apps/build.gradle.kts:  re-order ported-tutorial tasks

--- scala-apps/build.gradle.kts
+++ scala-apps/build.gradle.kts
@@ -36,13 +36,13 @@ tasks.register<JavaExec>("HelloJoltJni") {
 }
 
 // physics tutorial apps (very simple)
-tasks.register<JavaExec>("HelloCcd") {
-    description = "Runs the Scala port of the HelloCcd tutorial app."
-    mainClass = "com.github.stephengold.sportjolt.scala.tutorial.HelloCcd"
-}
 tasks.register<JavaExec>("HelloBroadPhase") {
     description = "Runs the Scala port of the HelloBroadPhase tutorial app."
     mainClass = "com.github.stephengold.sportjolt.scala.tutorial.HelloBroadPhase"
+}
+tasks.register<JavaExec>("HelloCcd") {
+    description = "Runs the Scala port of the HelloCcd tutorial app."
+    mainClass = "com.github.stephengold.sportjolt.scala.tutorial.HelloCcd"
 }
 tasks.register<JavaExec>("HelloCharacter") {
     description = "Runs the Scala port of the HelloCharacter tutorial app."
