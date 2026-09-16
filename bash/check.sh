390be3c2bb754528f8eca88035b4dce352f2c742
stephengold <sgold@sonic.net>
2026-09-16 10:19:58 -0700

    check.sh:  print version info

--- bash/check.sh
+++ bash/check.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+echo
+/usr/bin/lein --version
 /usr/bin/lein cljfmt fix
 
 cd jython-apps
 
+echo
+/usr/bin/black --version
 /usr/bin/black .
 
+echo
+~/.local/bin/ruff --version
 ~/.local/bin/ruff check --ignore E402,F821
